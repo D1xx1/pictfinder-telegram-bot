58d0e2ff6c5f283ed3c97e373c7e41b2efa0e492
Viktor <95516464+D1xx1@users.noreply.github.com>
2022-05-21 02:27:23 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 token = 'bot-token'
-quantity = int # число объектов для парсера
+quantity = int # число объектов для парсера (не стоит ставить много объектов)
 url = 'https://api.telegram.org/bot' # необходима для отправки сообщения через requests.get
